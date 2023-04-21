3c9e67c4a8b661ea6b9b7b8009259ae55844f8c8
Sonya Tkachenko <94766215+ssonyau@users.noreply.github.com>
2023-04-21 14:19:25 +0300

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,88 @@
-import { Component } from "react";
-import { SearchBar } from "./SearchBar/SearchBar";
-import { ImageGallery } from "./ImageGallery/ImageGallery";
-import { Modal } from "components/Modal/Modal";
-import { Loader } from "./Loder/Loader";
-import { Button } from "./Button/Button";
-import { getDataFromPixabay } from "js/pixabayApi";
-
-
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Oval } from 'react-loader-spinner';
+import  SearchBar  from './searchBar/searchBar.js';
+// import '../index.css';
+import ImageGallery from './imageGallery/imageGallery.js';
+import LoadMoreBtn from './button/button.js';
+import css from './app.module.css'
 
 export class App extends Component {
   state = {
-    searchKeyword: "",
-    bigImagePath: "",
-    renderImages: [],
-    page: 1,
+    images: [],
+    searchValue: '',
+    numberOfPage: 1,
     isLoading: false,
-    totalItems: 0
-  }
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.searchKeyword && (prevState.searchKeyword !== this.state.searchKeyword || prevState.page !== this.state.page)){
-        this.setState({
-            isLoading: true
-        })
-        getDataFromPixabay(this.state.searchKeyword, this.state.page)
-        .then(data => {
-            if(prevState.searchKeyword !== this.state.searchKeyword){
-                this.setState({
-                    renderImages: data.hits,
-                    totalItems: data.totalHits
-                })
-            }
-            else{
-                this.setState(prevState => {
-                    const newImages = prevState.renderImages.concat(data.hits);
-                    return {renderImages: newImages};
-                })
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        })
-        .finally(data => {
-            this.setState({
-                isLoading: false
-            })
-        })
-        
+    totalHits: 0,
+    error: false,
+  };
+
+  onSearchSubmit = value => {
+    this.setState({ searchValue: value });
+  };
+
+  incrementNumberOfPage = () => {
+    this.setState(prevState => {
+      return { numberOfPage: prevState.numberOfPage + 1 };
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchValue !== this.state.searchValue) {
+      this.setState({ isLoading: true, numberOfPage: 1})
+      return axios
+        .get(
+          `https://pixabay.com/api/?q=${this.state.searchValue}&page=1&key=33577731-7b9b7bf07a9d841c486c320f5&image_type=photo&orientation=horizontal&per_page=12`
+        )
+        .then(data =>
+          this.setState(prev => ({
+            ...prev,
+            images: data.data.hits,
+            isLoading: false,
+            totalHits: data.data.totalHits
+          }))
+        )
+        .catch(error => this.setState({error:true}))
+    }
+
+    if (prevState.numberOfPage < this.state.numberOfPage) {
+      this.setState({ isLoading: true });
+      return axios
+        .get(
+          `https://pixabay.com/api/?q=${this.state.searchValue}&page=${this.state.numberOfPage}&key=33577731-7b9b7bf07a9d841c486c320f5&image_type=photo&orientation=horizontal&per_page=12`
+        )
+        .then(data =>
+          this.setState(prevState => {
+            return {
+              images: [...prevState.images, ...data.data.hits],
+            };
+          })
+        )
+        .catch(error => this.setState({error:true}))
+        .finally(()=>{this.setState({isLoading: false})})
     }
   }
-  changeSerachKeyword = word => {
-    this.setState({
-      searchKeyword: word,
-      page: 1
-    })
-  }
-  setModalPath = (path) => {
-    this.setState({
-      bigImagePath: path
-    })
-  }
 
-  incrementPage = () => {
-    this.setState(prevState => {
-        return {page: prevState.page + 1}
-    })
-  }
-
-  render(){
+  render() {
+    const { images, isLoading, totalHits, error } = this.state;
     return (
-      <div className="App">
-        <SearchBar onSubmit={this.changeSerachKeyword}/>
-        {this.state.renderImages.length !== 0 && <ImageGallery renderImages={this.state.renderImages} setModalPath={this.setModalPath}/>}
-        {(this.state.renderImages.length !== 0 && this.state.renderImages.length < this.state.totalItems) && <Button onClick={this.incrementPage}/>}
-        {this.state.bigImagePath && <Modal onClick={this.setModalPath} path={this.state.bigImagePath}/>}
-        {this.state.isLoading && <Loader/>}
+      <div className={css.App}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        {error && <h1>Please try again</h1>}
+        {images.length > 0 && <ImageGallery images={images} />}
+        {images.length > 0 && !isLoading && images.length < totalHits && (
+          <LoadMoreBtn onClick={this.incrementNumberOfPage} />
+        )}
+        {isLoading && (
+          <Oval
+            height={180}
+            width={180}
+            color="#3f51b5"
+            secondaryColor="#3f51b5"
+            wrapperClass="OvalWrapper"
+          />
+        )}
       </div>
     );
   }
-};
+}
